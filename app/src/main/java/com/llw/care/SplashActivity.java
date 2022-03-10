8d805f21d228e0c6d7423c41e3a753a379104a53
LiLongwei <lonelyholiday@qq.com>
2022-03-10 16:31:12 +0800

    Initial commit

---
+++ app/src/main/java/com/llw/care/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.llw.care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> startActivity(new Intent(this,MainActivity.class)),500);
+    }
+}
